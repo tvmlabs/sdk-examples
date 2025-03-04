c70ccdd1859b17deb1480d6393c3240b83318c67
Oxana Selivanova <39991771+Oxydixi@users.noreply.github.com>
2025-03-04 11:00:33 +0100

    Update js example (#4)

--- js/nodejs/helloWorld/resources/helloWorld.js
+++ js/nodejs/helloWorld/resources/helloWorld.js
@@ -2,11 +2,21 @@ module.exports = {
     helloWorld: {
         abi: {
             "ABI version": 2,
+            "version": "2.4",
             "header": ["time", "expire"],
             "functions": [
                 {
                     "name": "constructor",
                     "inputs": [
+                        {"name":"value","type":"uint64"}
+                    ],
+                    "outputs": [
+                    ]
+                },
+                {
+                    "name": "exchangeToken",
+                    "inputs": [
+                        {"name":"value","type":"uint64"}
                     ],
                     "outputs": [
                     ]
@@ -16,7 +26,7 @@ module.exports = {
                     "inputs": [
                     ],
                     "outputs": [
-                        {"name":"value0","type":"bytes"}
+                        {"name":"value0","type":"string"}
                     ]
                 },
                 {
@@ -27,19 +37,38 @@ module.exports = {
                     ]
                 },
                 {
-                    "name": "getTimestamp",
+                    "name": "callExtTouch",
                     "inputs": [
+                        {"name":"addr","type":"address"}
                     ],
                     "outputs": [
-                        {"name":"value0","type":"uint256"}
                     ]
                 },
                 {
-                    "name": "sendValue",
+                    "name": "sendVMShell",
                     "inputs": [
                         {"name":"dest","type":"address"},
                         {"name":"amount","type":"uint128"},
                         {"name":"bounce","type":"bool"}
+                    ],
+                    "outputs": [
+                    ]
+                },
+                {
+                    "name": "sendShell",
+                    "inputs": [
+                        {"name":"dest","type":"address"},
+                        {"name":"value","type":"uint128"}
+                    ],
+                    "outputs": [
+                    ]
+                },
+                {
+                    "name": "deployNewContract",
+                    "inputs": [
+                        {"name":"stateInit","type":"cell"},
+                        {"name":"initialBalance","type":"uint128"},
+                        {"name":"payload","type":"cell"}
                     ],
                     "outputs": [
                     ]
@@ -53,11 +82,15 @@ module.exports = {
                     ]
                 }
             ],
-            "data": [
-            ],
             "events": [
+            ],
+            "fields": [
+                {"init":true,"name":"_pubkey","type":"uint256"},
+                {"init":false,"name":"_timestamp","type":"uint64"},
+                {"init":false,"name":"_constructorFlag","type":"bool"},
+                {"init":false,"name":"timestamp","type":"uint32"}
             ]
         },
-        tvc: "te6ccgECGQEAAtgAAgE0AwEBAcACAEPQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgBCSK7VMg4wMgwP/jAiDA/uMC8gsWBQQYApAh2zzTAAGOEoECANcYIPkBWPhCIPhl+RDyqN7TPwH4QyG58rQg+COBA+iogggbd0CgufK0+GPTHwH4I7zyudMfAds8+Edu8nwJBgE6ItDXCwOpOADcIccA3CHXDR/yvCHdAds8+Edu8nwGAiggghBU1r0Yu+MCIIIQaLVfP7vjAgsHAiggghBoF+U1uuMCIIIQaLVfP7rjAgoIAlgw+EJu4wD4RvJzf/hm0fhC8uBl+EUgbpIwcN74Qrry4Gb4APgj+GrbPH/4ZwkTAUrtRNDXScIBio4acO1E0PQFcPhqgED0DvK91wv/+GJw+GNw+GbiFQFSMNHbPPhKIY4cjQRwAAAAAAAAAAAAAAAAOgX5TWDIzssfyXD7AN5/+GcVBFAgghAfnWSDuuMCIIIQNzEuRbrjAiCCEDtj1H664wIgghBU1r0YuuMCEhEPDAJsMNHbPCGOJyPQ0wH6QDAxyM+HIM6NBAAAAAAAAAAAAAAAAA1Na9GIzxbMyXD7AJEw4uMAf/hnDRMBAogOABRoZWxsb1dvcmxkA1Yw+EJu4wD6QZXU0dD6QN/XDX+V1NHQ03/f1wwAldTR0NIA39HbPOMAf/hnFRATAFT4RSBukjBw3vhCuvLgZvgAVHEgyM+FgMoAc89AzgH6AoBrz0DJcPsAXwMCJDD4Qm7jANH4APgj+GrbPH/4ZxUTA3gw+EJu4wDR2zwhjigj0NMB+kAwMcjPhyDOjQQAAAAAAAAAAAAAAAAJ+dZIOM8Wy//JcPsAkTDi4wB/+GcVFBMAKPhK+Eb4Q/hCyMv/yz/KAMsfye1UAAT4SgAo7UTQ0//TP9IA0x/R+Gr4Zvhj+GICCvSkIPShGBcAFnNvbCAwLjQ2LjANAAA=",
+        tvc: "te6ccgECIQEAA9sAAgE0AgEAWXm9Qxs+1m6mBHy4QCWMLUf34ukuUJG1tv/l0dApYroiAAAAAAAAAAAAAAAAQAQkiu1TIOMDIMD/4wIgwP7jAvILHgQDIAKUIds80wABjheDCNcYIPgoyM7OyfkAWPhCIPhl+RDyqN7TPwH4QyG58rQg+COBA+iogggbd0CgufK0+GPTHwH4I7zyudMfAds88jwYBQNS7UTQgQFA1yHXCgD4ZiLQ1wsDqTgA3CHHAOMCIdcNH/K8IeMDAds88jwdHQUEUCCCEDcxLkW74wIgghBwPeLPu+MCIIIQdtKRnLrjAiCCEHyaeH664wIPCggGAh4w+Eby4EzTP9HbPOMA8gAHGgEM+ADbPMcnFwM0MPhG8uBM+EJu4wAhk9TR0N76QNHbPOMA8gAYCRoBavhFIG6SMHDe+EK68uBm+ADbPMjPhYjOjQVOYloAAAAAAAAAAAAAAAAAAA3MS5FgzxbJcPsAFwM8IIIQaBflNbrjAiCCEGtJAFm64wIgghBwPeLPuuMCDgwLAjww+EJu4wD4RvJz0z/Rxyf4QvLgZfgA+CP4ats88gAYGgM8MPhG8uBM+EJu4wAhk9TR0N76QNN/0gDR2zzjAPIAGA0aAVb4RSBukjBw3vhCuvLgZvgA2zwBtXdZyM+FgMoAz4RAzgH6AoBsz0DJcPsAFwFQMNHbPPhKIY4cjQRwAAAAAAAAAAAAAAAAOgX5TWDIzssfyXD7AN7yABgEUCCCEAc0LEq64wIgghAeSP9RuuMCIIIQJzhZZbrjAiCCEDcxLkW64wIZFRIQAyQw+Eby4Ez4Qm7jANHbPNs88gAYERoBEPgA2zz4I/hqFwIiMPhG8uBM1NN/1NHbPOMA8gATGgJc+ADbPCL5AMjPigBAy//J0Fi1dwHIz4WIzgH6AnPPC2tUECDbPM8Uz4PMyXD7ABcUADTQ0gABk9IEMd7SAAGT0gEx3vQE9AT0BNFfAwM4MPhG8uBM+EJu4wAhk9TR0N76QNN/0ds84wDyABgWGgJo+EUgbpIwcN74Qrry4Gb4ANs8cm3IVQL6BlmAIPRDAcjPhYjOz4Qg9ABxzwtqiM8UyXH7ABcgACz4J28QghgXSHboALzcghgXSHboAMcoACjtRNDT/9M/0wDTH9H4avhm+GP4YgJcMPhG8uBM0ds8IY4bI9DTAfpAMDHIz4cgzoIQhzQsSs8LgczJcPsAkTDi4wDyABsaACT4SvhD+ELIy//LP8+Dyx/J7VQBAogcABRoZWxsb1dvcmxkAAr4RvLgTAIQ9KQg9L3ywE4gHwAUc29sIDAuNzYuMQAA",
     }
 }
